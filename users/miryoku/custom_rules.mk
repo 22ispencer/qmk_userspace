237622e3f08bf8666a4dedc8039931836bd3b7a7
Isaac Spencer <22ispencer@gmail.com>
2024-12-31 08:21:32 -0600

    custom config for crkbd

--- users/miryoku/custom_rules.mk
+++ users/miryoku/custom_rules.mk
@@ -1,3 +1,9 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
+MIRYOKU_ALPHAS     = COLEMAK
+MIRYOKU_EXTRA      = QWERTY
+VIA_ENABLE         = yes
+VIAL_ENABLE        = yes
+VIALRGB_ENABLE     = yes
+ENCODER_MAP_ENABLE = yes
